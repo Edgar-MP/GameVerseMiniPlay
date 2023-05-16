166cbaa9391f311440fd6705a568bd21a843320c
Edgar <57589251+Edgar-MP@users.noreply.github.com>
2023-05-16 20:48:12 +0200

    New games

--- src/assets/guessTheScreenshotOriginal.js
+++ src/assets/guessTheScreenshotOriginal.js
@@ -52,5 +52,70 @@ export const gameList = {
         "id" : "0dR7MebPIua4ZPnH0HlH",
         "day" : "18/05/2023",
         "played" : false
-    }
+    },
+    "9": {
+        "id" : "ndu2Pw9nYzIRZP4YXbGF",
+        "day" : "19/05/2023",
+        "played" : false
+    },
+    "10": {
+        "id" : "SMERcQgworeir9Nl4vLU",
+        "day" : "20/05/2023",
+        "played" : false
+    },
+    "11": {
+        "id" : "xsT7KgOmnh0ST2y4DKEQ",
+        "day" : "21/05/2023",
+        "played" : false
+    },
+    "12": {
+        "id" : "ZAy44aVBSBggOdJC8fab",
+        "day" : "22/05/2023",
+        "played" : false
+    },
+    "13": {
+        "id" : "kSFcx0liOJmHlGsMGRdG",
+        "day" : "23/05/2023",
+        "played" : false
+    },
+    "14": {
+        "id" : "4bK3MhQ7hDRjuZW1D2Uh",
+        "day" : "24/05/2023",
+        "played" : false
+    },
+    "15": {
+        "id" : "4rspByJn5RsZEODjRU15",
+        "day" : "25/05/2023",
+        "played" : false
+    },
+    "16": {
+        "id" : "Ro7O5TS4zrLQTb5XCAId",
+        "day" : "26/05/2023",
+        "played" : false
+    },
+    "17": {
+        "id" : "yAORhwA0vBsP3x9YX3aT",
+        "day" : "27/05/2023",
+        "played" : false
+    },
+    "18": {
+        "id" : "gbdpz8PfXQb9XkXEzbBn",
+        "day" : "28/05/2023",
+        "played" : false
+    },
+    "19": {
+        "id" : "HHtNmEFJaVc9aTEYpPH3",
+        "day" : "29/05/2023",
+        "played" : false
+    },
+    "20": {
+        "id" : "NgL25pqyTDYOmuVF50JE",
+        "day" : "30/05/2023",
+        "played" : false
+    },
+    "21": {
+        "id" : "uatYE1JnVKV5A4umvCiH",
+        "day" : "31/05/2023",
+        "played" : false
+    },
 }
